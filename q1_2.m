5e2b9152886df709454a662fa7c6488dd016637d
Vamsidhar Muthireddy <vamsidhar.muthireddy@research.iiit.ac.in>
2017-11-09 21:06:28 +0530

    q1_2 decent

--- q1_2.m
+++ q1_2.m
@@ -7,16 +7,16 @@ in = im2double(in);
 
 k1=0.01;
 
-k2=0.0025;
+k2=0.003;
 out_turb = restore_turb(in,k1,k2);
 
 T=1;
 a=1e-8 ;
-b=0.004;
-out_motion = restore_motion(in,k1,T,a,b);
+b=0.009;
+out_motion = restore_weiner(in,k1,T,a,b);
 
 len=10;
-theta=0;
+theta=90;
 out_motion_2 = restore_motion_2(in,k1,len,theta);
 
 
